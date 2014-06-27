fd7e026dfcbffea754a74b99d35e4baec4f00756
Alicia Smith <aliciaportersmith@gmail.com>
2014-06-26 21:35:05 -0600

    localhost
    switch db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140624042129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
